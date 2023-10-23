8f1c290a3be925d96c247095c208f9447c1d8f48
Nguyen Thanh Nghi <thanhnghi1421@gmail.com>
2023-10-23 22:26:52 +0700

    create customer page

--- src/pages/customer/CreateCustomer.tsx
+++ src/pages/customer/CreateCustomer.tsx
@@ -1,7 +1,138 @@
+import { Button, Grid, Paper, Typography } from "@mui/material"
+import Address from "../../components/Address"
+import { FormInputText } from "../../components/form/FormInputText"
+import { FormInputDropdown } from "../../components/form/FormInputDropdown"
+import yup from "../../utils/yup";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { genders } from "../../utils/constants";
+import { yupResolver } from "@hookform/resolvers/yup";
 
+interface CustomerForm {
+    name: string;
+    phoneNumber: string;
+    gender: string;
+    email: string;
+}
+
+const defaultValues = {
+    name: "",
+    phoneNumber: "",
+    gender: '1',
+    email: '',
+};
+
+const maxDate = new Date()
+maxDate.setFullYear(new Date().getFullYear() - 18)
+
+const customerFormValidate = yup.object({
+    name: yup
+        .string()
+        .required('Tên nhân viên bắt buộc.')
+        .max(255),
+    phoneNumber: yup
+        .string()
+        .required('Số điện thoại bắt buộc.')
+        .phoneNumber('Số điện thoại không hợp lệ.'),
+    email: yup
+        .string()
+        .required('Email bắt buộc.')
+        .email('Email không hợp lệ.'),
+    gender: yup
+        .string()
+        .required('Giới tính bắt buộc')
+        .oneOf(['0', '1', '2']),
+})
 const CreateCustomer: React.FC = () => {
+    const navigate = useNavigate()
+    const [address, setAddress] = useState<string>('')
+
+    const { handleSubmit, watch, reset, control, setValue } = useForm<CustomerForm>({
+        defaultValues: defaultValues,
+        resolver: yupResolver(customerFormValidate)
+    });
+
+    const onSubmit = (data: CustomerForm) => console.log(data);
+
+    const backToTable = () => {
+        navigate('/custumers')
+    }
     return (
-        <>this is create customer page</>
+        <Paper sx={{ px:6, py:4 }}>
+            <Typography variant="h6" gutterBottom mb='20px'>
+                Thông tin khách hàng
+            </Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={8} sm={4}>
+                    <FormInputText
+                        name="name"
+                        control={control}
+                        label="Họ và tên"
+                        placeholder='Nhập họ và tên khách hàng'
+                    />
+                </Grid>
+                <Grid item xs={8} sm={4}>
+                    <FormInputText
+                        name="phoneNumber"
+                        control={control}
+                        label="Số điện thoại"
+                        placeholder='Nhập họ số điện thoại'
+                    />
+                </Grid>
+                
+                <Grid item xs={8} sm={4}>
+                    <FormInputText
+                        name="email"
+                        control={control}
+                        label="Email"
+                        placeholder='Nhập email khách hàng'
+                    />
+                </Grid>
+                    
+                <Address setAddress={setAddress} />  
+
+                <Grid item xs={8} sm={2}>
+                    <FormInputDropdown
+                        name="gender"
+                        control={control}
+                        label="Giới tính"
+                        placeholder='Giới tính'
+                        list={genders}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} container 
+                    sx={{
+                        display: 'flex',
+                        justifyContent: "end",
+                        gap: 2
+                    }}
+                >
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        sx={{
+                            textTransform: 'none',
+                        }}
+                        onClick={handleSubmit(onSubmit)}
+                    >
+                        Thêm
+                    </Button>
+
+                    <Button
+                        variant="contained"
+                        color="error"
+                        sx={{
+                            textTransform: 'none',
+                        }}
+                        onClick={backToTable}
+                    >
+                        Quay về
+                    </Button>
+                </Grid>
+            </Grid>
+        </Paper>
     )
 }
 
--- src/pages/staff/CreateStaff.tsx
+++ src/pages/staff/CreateStaff.tsx
@@ -19,11 +19,9 @@ import { genders } from '../../utils/constants'
 import { FormInputDropdown } from "../../components/form/FormInputDropdown";
 import yup from '../../utils/yup'
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Item } from "../../types/props/FormInputListProps";
 import Address from "../../components/Address";
 import { FormInputDate } from "../../components/form/FormInputDate";
 import { FormInputCheckBox } from "../../components/form/FormInputCheckBox";
-import dayjs from "dayjs";
 
 interface StaffForm {
     name: string;
@@ -93,133 +91,134 @@ const CreateStaff: React.FC = () => {
     }
 
     return (
-    <Paper sx={{ px:6, py:4 }}>
-        <Typography variant="h6" gutterBottom mb='20px'>
-            Thông tin nhân viên
-        </Typography>
-        <Grid container spacing={3}>
-            <Grid item xs={8} sm={5}>
-                <FormInputText
-                    name="name"
-                    control={control}
-                    label="Họ và tên"
-                    placeholder='Nhập họ và tên nhân viên'
-                />
-            </Grid>
-            <Grid item xs={8} sm={5}>
-                <FormInputText
-                    name="phoneNumber"
-                    control={control}
-                    label="Số điện thoại"
-                    placeholder='Nhập họ số điện thoại'
-                />
-            </Grid>
-            <Grid item xs={8} sm={2}>
-                <FormInputDropdown
-                    name="gender"
-                    control={control}
-                    label="Giới tính"
-                    placeholder='Giới tính'
-                    list={genders}
-                />
-            </Grid>
-            <Grid item xs={8} sm={4}>
-                <FormInputText
-                    name="email"
-                    control={control}
-                    label="Email"
-                    placeholder='Nhập email nhân viên'
-                />
-            </Grid>
-            <Grid item xs={8} sm={4}>
-                <FormInputText
-                    name="identification"
-                    control={control}
-                    label="CCCD"
-                    placeholder='Nhập số CCCD nhân viên'
-                />
-            </Grid>
-          
-            <Grid item xs={8} sm={4}>
-                <FormControl fullWidth>
-                    <InputLabel >Chức vụ</InputLabel>
-                    <Controller
-                        render={({ field: { onChange, value } }) => (
-                                <Select
-                                    onChange={onChange}
-                                    value={value}
-                                    label='Chức vụ'
-                                >
-                                    {
-                                        isLoading
-                                            ?
-                                            <CircularProgress sx={{ margin: 'auto' }} />
-                                            :
-                                            data.data.map((position) => (<MenuItem value={position.id}>
-                                                    {position.name}
-                                                </MenuItem>)
-                                            )
-                                    }
-                            </Select>
-                        )}
+        <Paper sx={{ px:6, py:4 }}>
+            <Typography variant="h6" gutterBottom mb='20px'>
+                Thông tin nhân viên
+            </Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={8} sm={5}>
+                    <FormInputText
+                        name="name"
                         control={control}
-                        name='positionId'
-                    />    
-      
-                </FormControl>
-            </Grid>
-                
-            <Address setAddress={setAddress} />  
-                
-            <Grid item xs={8} sm={4}>
-                <FormInputDate
-                    name="dob"
-                    control={control}
-                    label="Ngày sinh"
-                    placeholder='x'
-                />
-            </Grid>
-            
-            <Grid item xs={12} sm={12}>
-                <FormInputCheckBox
-                    name="isWorking"
-                    control={control}
-                    label="Nhân viên đang làm việc"
-                    placeholder='x'
+                        label="Họ và tên"
+                        placeholder='Nhập họ và tên nhân viên'
                     />
-            </Grid>
+                </Grid>
+                <Grid item xs={8} sm={5}>
+                    <FormInputText
+                        name="phoneNumber"
+                        control={control}
+                        label="Số điện thoại"
+                        placeholder='Nhập họ số điện thoại'
+                    />
+                </Grid>
+                <Grid item xs={8} sm={2}>
+                    <FormInputDropdown
+                        name="gender"
+                        control={control}
+                        label="Giới tính"
+                        placeholder='Giới tính'
+                        list={genders}
+                    />
+                </Grid>
+                <Grid item xs={8} sm={4}>
+                    <FormInputText
+                        name="email"
+                        control={control}
+                        label="Email"
+                        placeholder='Nhập email nhân viên'
+                    />
+                </Grid>
+                <Grid item xs={8} sm={4}>
+                    <FormInputText
+                        name="identification"
+                        control={control}
+                        label="CCCD"
+                        placeholder='Nhập số CCCD nhân viên'
+                    />
+                </Grid>
+            
+                <Grid item xs={8} sm={4}>
+                    <FormControl fullWidth>
+                        <InputLabel >Chức vụ</InputLabel>
+                        <Controller
+                            render={({ field: { onChange, value } }) => (
+                                    <Select
+                                        onChange={onChange}
+                                        value={value}
+                                        label='Chức vụ'
+                                    >
+                                        {
+                                            isLoading
+                                                ?
+                                                <CircularProgress sx={{ margin: 'auto' }} />
+                                                :
+                                                data.data.map((position: any) => (<MenuItem value={position.id}>
+                                                        {position.name}
+                                                    </MenuItem>)
+                                                )
+                                        }
+                                </Select>
+                            )}
+                            control={control}
+                            name='positionId'
+                        />    
+        
+                    </FormControl>
+                </Grid>
+                    
+                <Address setAddress={setAddress} />  
+                    
+                <Grid item xs={8} sm={4}>
+                    <FormInputDate
+                        name="dob"
+                        control={control}
+                        label="Ngày sinh"
+                        placeholder='x'
+                    />
+                </Grid>
+                
+                <Grid item xs={12} sm={12}>
+                    <FormInputCheckBox
+                        name="isWorking"
+                        control={control}
+                        label="Nhân viên đang làm việc"
+                        placeholder='x'
+                        />
+                </Grid>
 
-            <Grid item xs={12} sm={12} container 
-                sx={{
-                    display: 'flex',
-                    justifyContent: "end",
-                    gap: 2
-                }}
-            >
-                <Button
-                    variant="contained"
-                    color="primary"
+                <Grid item xs={12} sm={12} container 
                     sx={{
-                        textTransform: 'none',
+                        display: 'flex',
+                        justifyContent: "end",
+                        gap: 2
                     }}
-                    onClick={handleSubmit(onSubmit)}
                 >
-                    Thêm
-                </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        sx={{
+                            textTransform: 'none',
+                        }}
+                        onClick={handleSubmit(onSubmit)}
+                    >
+                        Thêm
+                    </Button>
 
-                <Button
-                    variant="contained"
-                    color="error"
-                    sx={{
-                        textTransform: 'none',
-                    }}
-                    onClick={backToTable}
-                >
-                    Quay về
-                </Button>
+                    <Button
+                        variant="contained"
+                        color="error"
+                        sx={{
+                            textTransform: 'none',
+                        }}
+                        onClick={backToTable}
+                    >
+                        Quay về
+                    </Button>
+                </Grid>
             </Grid>
-        </Grid>
-    </Paper>)
+        </Paper>
+    )
 }
 
 export default CreateStaff
