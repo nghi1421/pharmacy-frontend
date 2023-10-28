97a2d9a3f337238ddcd468e4c2bbb33393ee2f3d
Nguyen Thanh Nghi <thanhnghi1421@gmail.com>
2023-10-29 03:20:11 +0700

    empty data table

--- src/components/table/TableComponent.tsx
+++ src/components/table/TableComponent.tsx
@@ -1,5 +1,7 @@
-import { Box, Divider, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow, Typography, styled, tableCellClasses } from "@mui/material"
+import { Box, Divider, Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow, Typography, styled, tableCellClasses } from "@mui/material"
 import { Column } from '../../types/Column'
+import React from "react"
+import EmptyImage from '../../assets/images/no-data.jpg'
 // import { TablePaginationActions } from "./TablePaginationActions";
 
 interface TableProps<T> {
@@ -54,71 +56,109 @@ const TableComponent: React.FC<TableProps<any>> = ({ rows, keyTable, columns, ac
   
   return (
       <TableContainer component={Paper}>
-        <Divider/>
+      <Divider />
+          {
+            rows.length === 0
+            ?
+          <React.Fragment>
+            <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            >
+              <Grid item xs={3}>
+                  <Box
+                    component="img"
+                      sx={{
+                      
+                      height: 200,
+                      width: 200,
+                    }}
+                    alt="No data."
+                    src={EmptyImage}
+                  />
+              </Grid>
+              <Grid item xs={4}>
+                  <Typography
+                    variant="h6"
+                    fontWeight='500'
+                    
+                  sx={{ opacity: '0.3', pb: 4}}
+                >
+                  Không có dữ liệu
+                </Typography>
+              </Grid>      
+              </Grid>
+              </React.Fragment>
+            :
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow key={keyTable}>
-                        {columns.map((column, index) => (
-                          <StyledTableCell
-                            align="left"
-                            key={`header-cell-${keyTable}-${index}`}
-                          >
-                            {column.value}
-                          </StyledTableCell>
-                        ))}
-                    {
-                      hasAction
-                        ? <StyledTableCell
-                            align="left"
-                            key={`header-cell-${keyTable}-${columns.length +1}`}
-                          >
-                            Hành động
-                        </StyledTableCell>
-                        : ''
-                        }
-                    </TableRow>
-                </TableHead>
+                    <TableHead>
+                        <TableRow key={keyTable}>
+                            {columns.map((column, index) => (
+                              <StyledTableCell
+                                align="left"
+                                key={`header-cell-${keyTable}-${index}`}
+                              >
+                                {column.value}
+                              </StyledTableCell>
+                            ))}
+                        {
+                          hasAction
+                            ? <StyledTableCell
+                                align="left"
+                                key={`header-cell-${keyTable}-${columns.length +1}`}
+                              >
+                                Hành động
+                            </StyledTableCell>
+                            : ''
+                            }
+                        </TableRow>
+                    </TableHead>
 
-                <TableBody>
-                    {rows.map((row, rowIndex) => (
-                    <StyledTableRow
-                        key={`row-${keyTable}-${rowIndex}`}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-                        {columns.map((column, index) => (
-                          <TableCell
-                            align="left"
-                            key={`row-${rowIndex}-${keyTable}-${index}`}>
-                            {row[column.key]}
-                          </TableCell>
+                    <TableBody>
+                        {rows.map((row, rowIndex) => (
+                        <StyledTableRow
+                            key={`row-${keyTable}-${rowIndex}`}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            {columns.map((column, index) => (
+                              <TableCell
+                                align="left"
+                                key={`row-${rowIndex}-${keyTable}-${index}`}>
+                                {row[column.key]}
+                              </TableCell>
+                            ))}
+                            {hasAction ? action(row) : ''}
+                            
+                        </StyledTableRow>
                         ))}
-                        {hasAction ? action(row) : ''}
-                        
-                    </StyledTableRow>
-                    ))}
-                </TableBody>
-        
-                    {/* <TableFooter>
-                      <TableRow>
-                        <TablePagination
-                          rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                          colSpan={3}
-                          count={rows.length}
-                          rowsPerPage={rowsPerPage}
-                          page={page}
-                          SelectProps={{
-                            inputProps: {
-                              'aria-label': 'rows per page',
-                            },
-                            native: true,
-                          }}
-                          onPageChange={handleChangePage}
-                          onRowsPerPageChange={handleChangeRowsPerPage}
-                          // ActionsComponent={TablePaginationActions}
-                        />
-                      </TableRow>
-                    </TableFooter> */}
-            </Table>
+                    </TableBody>
+            
+                        {/* <TableFooter>
+                          <TableRow>
+                            <TablePagination
+                              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                              colSpan={3}
+                              count={rows.length}
+                              rowsPerPage={rowsPerPage}
+                              page={page}
+                              SelectProps={{
+                                inputProps: {
+                                  'aria-label': 'rows per page',
+                                },
+                                native: true,
+                              }}
+                              onPageChange={handleChangePage}
+                              onRowsPerPageChange={handleChangeRowsPerPage}
+                              // ActionsComponent={TablePaginationActions}
+                            />
+                          </TableRow>
+                        </TableFooter> */}
+                </Table>
+          }
+            
         </TableContainer>
     )
 }
